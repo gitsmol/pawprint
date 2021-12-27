2303cd12c6e509c988b6e31d23950333f06737f4
Geert Smolders <gsmolders@posteo.net>
2021-12-27 14:37:55 +0100

    refactoring done

--- pawprint.py
+++ pawprint.py
@@ -16,12 +16,7 @@ class BearableData:
         
         self.REP_dates = pd.DataFrame(self.STA_df['date'].unique(), columns=['date'])
         self.REP_longform = self.build_longform(self.INT_df, self.categories)
-        # self.REP_symptoms = self.build_REP_df(self.STA_df, 'Symptom')
-        # self.REP_energy= self.build_REP_df(self.STA_df, 'Energy')
-        # self.REP_sleep = self.build_REP_df(self.STA_df, 'Sleep')
-        # self.REP_sleep_quality = self.build_REP_df(self.STA_df, 'Sleep quality')
-        # self.REP_mood = self.build_REP_df(self.STA_df, 'Mood')
-        # self.REP_factors = self.build_REP_df(self.STA_df, 'Factors')
+        
         
     def wrangle(self, df):
         # Timestamps are a mess in bearable export data. (It's true, sorry guys and gals.)
@@ -111,6 +106,7 @@ class BearableData:
             df_longform = df_longform.append(df_category)
 
         df_longform = df_longform[['datetime', 'category', 'factor', 'rating/amount', 'average', 'detail']]
+        df_longform.sort_values(by='datetime', inplace=True)
         return df_longform
 
 def draw_bearable_fig(data):
@@ -119,36 +115,23 @@ def draw_bearable_fig(data):
     colors = ['#00429d', '#4b568d', '#6c6a7a', '#ff0000', '#fdd249', '#ffa563', '#e06dff']
 
     for category in data.categories:
-        print(f'category is {category}')
         selection = data.REP_longform.loc[(data.REP_longform['category'] == category)]
-        trace_data = px.scatter(x=selection['datetime'], y=selection['rating/amount'], trendline="rolling", trendline_options={'window': 7}).data[1]
-        # create a scatterplot with a trendline. Then take only that data[1] from the graph.
-        # symp_trend = px.scatter(x=data_obj.REP_symptoms['date'], y=data_obj.REP_symptoms['rating/amount'], trendline="rolling", trendline_options={'window': 15}, ).data[1]
-    # energy_trend = px.scatter(x=data_obj.REP_energy['date'], y=data_obj.REP_energy['rating/amount'], trendline="rolling", trendline_options={'window': 15}, ).data[1]
 
-    # symp_trend = px.scatter(x=data_obj.REP_longform['date'], y=data.REP_longform['rating/amount'], trendline="rolling", trendline_options={'window': 7})
-        trace_name = f'{category} trend'
-        fig_measurements.add_trace(go.Scatter(trace_data, name=trace_name, line={'color' : colors[0]}, showlegend=True, line_shape='spline', fill='tozeroy'))
-    # fig.add_trace(go.Scatter(energy_trend, name='Energy Trend', line={'color' : colors[2]}, showlegend=True, line_shape='spline', fill='tozeroy'))
+        if category == 'Factors':
+            fig_factors = go.Figure()
+            for factor in data.factors_unique:
+                df_factor = selection.loc[selection['factor'] == factor]
+                fig_factors.add_trace(go.Histogram(x=df_factor['datetime'], y=df_factor['rating/amount'], name = factor, histfunc='sum', xbins=dict(size= '604800000'), autobinx=False) # 1 week in milliseconds
+                        )
+        
+        else:
+            trace_data = px.scatter(x=selection['datetime'], y=selection['rating/amount'], trendline="rolling", trendline_options={'window': 7}).data[1]
+            
+            trace_name = f'{category} moving'
+            # fig_measurements.add_trace(go.Scatter(trace_data, name=trace_name, line={'color' : colors[0]}, showlegend=True, line_shape='spline', fill='tozeroy'))
+            fig_measurements.add_trace(go.Scatter(x=selection['datetime'], y=selection['rating/amount'], name=category, showlegend=True, line_shape='spline', fill='tozeroy'))
+            fig_measurements.add_trace(go.Scatter(x=selection['datetime'], y=selection['average'], name=trace_name, showlegend=True, line_shape='spline', fill='tozeroy'))
 
-    # # fig.add_trace(go.Histogram(x=data_obj.REP_mood['date'], y=data_obj.REP_mood['rating/amount'],
-    #                     name='Mood',
-    #                     histfunc='avg',
-    #                     marker_color=colors[3],
-    #                     opacity=0.6,
-    #                     xbins=dict(
-    #                       size=604800000), # 1 week in milliseconds
-    #                       autobinx=False
-    #                     ))
-    # fig.add_trace(go.Histogram(x=data_obj.REP_sleep['date'], y=data_obj.REP_sleep['rating/amount'],
-                     # name='Sleep',
-                     # histfunc="avg",
-                     # opacity=0.6,
-                     # marker_color = colors[4],
-                     # xbins=dict(
-                     #  size=604800000), # 1 week in milliseconds
-                     #  autobinx=False
-                     #  ))
 
     fig_measurements.update_layout(
         width=900,
@@ -159,22 +142,8 @@ def draw_bearable_fig(data):
         # barmode='overlay'
         )
     fig_measurements.update_xaxes(
-      rangeslider_visible = True
+    rangeslider_visible = True
     )
-
-    fig_factors = go.Figure()
-    for factor in data_obj.factors_unique:
-        fig_factors.add_trace(go.Histogram(x=data_obj.REP_factors['date'], y=data_obj.REP_factors[factor],
-                        name = factor,
-                        histfunc='sum',
-                        # legendgroup='Factors',
-                        visible=True,
-                        xbins=dict(
-                          size=604800000), # 1 week in milliseconds
-                          autobinx=False
-                        )
-
-                        )
 
     return fig_measurements, fig_factors
 
